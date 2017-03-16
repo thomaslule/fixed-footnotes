6d3a75698d1d0961ed649fd626328b6e4d67831e
unknown <thomas@lule.fr>
2017-03-16 17:57:47 +0100

    add css to build job

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 var path = require('path');
 
 module.exports = {
   entry: './src/fixed-footnotes.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'fixed-footnotes.js',
     path: path.resolve(__dirname, 'build'),
     library: "fixedFootnotes"
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([{ from: "./src/fixed-footnotes.css" }])
+  ]
 };
