8d544dab4b402c286cf9df93d9e834c95cacac8b
unknown <thomas@lule.fr>
2017-05-13 12:15:39 +0200

    add a playground to test things when developping

---
+++ playground/demo.js
@@ -0,0 +1,27 @@
+hljs.initHighlightingOnLoad();
+
+document.getElementById("code-theme").style.display = "none";
+
+var ffn = fixedFootnotes();
+
+document.getElementById("button-no-theme").onclick = function() {
+  document.getElementById("code-theme").style.display = "none";
+  document.getElementById("code-no-theme").style.display = "";
+  ffn.stop();
+  document.getElementById("ffn-theme").setAttribute("href", "../build/fixed-footnotes.css");
+  ffn = fixedFootnotes();
+}
+
+document.getElementById("button-theme").onclick = function() {
+  document.getElementById("code-theme").style.display = "";
+  document.getElementById("code-no-theme").style.display = "none";
+  ffn.stop();
+  document.getElementById("ffn-theme").setAttribute("href", "theme.css");
+  ffn = fixedFootnotes({
+    fixedContainerLocation: "#column",
+    transformNote: function(note) {
+      note.getElementsByTagName("span")[0].firstChild.textContent += " and I modified it";
+      return note;
+    }
+  });
+}
